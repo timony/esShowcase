2e0d28d3dba76bdf2b161508a39189d931308937
Tomas Mika <tmika@vendavo.com>
2019-12-15 23:49:37 +0100

    Added Jackson object mapper bean

---
+++ src/main/java/com/vendavo/tmika/priceoptimizationes/configuration/JacksonConfiguration.java
@@ -0,0 +1,18 @@
+package com.vendavo.tmika.priceoptimizationes.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        return objectMapper;
+    }
+}
