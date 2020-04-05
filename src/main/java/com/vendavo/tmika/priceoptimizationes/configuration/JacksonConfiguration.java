42463fb436004d01ff43776e917acddee12334a9
Tomas Mika <tmika@vendavo.com>
2020-04-05 21:24:47 +0200

    Fixed query with getting all results

--- src/main/java/com/vendavo/tmika/priceoptimizationes/configuration/JacksonConfiguration.java
+++ src/main/java/com/vendavo/tmika/priceoptimizationes/configuration/JacksonConfiguration.java
@@ -1,5 +1,6 @@
 package com.vendavo.tmika.priceoptimizationes.configuration;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -15,7 +16,7 @@ public class JacksonConfiguration {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_CONCRETE_AND_ARRAYS, JsonTypeInfo.As.PROPERTY);
         objectMapper.registerModule(new Jdk8Module());
         return objectMapper;
     }
